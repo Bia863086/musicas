b237b0f1db2b33c78bda71be6ccc303117dda117
Bia863086 <beatrizcampos2101@gmail.com>
2021-08-29 21:29:27 -0300

    Um trecho de uma música que eu gosto, adicionei cores e dupliquei a linha de comando WriteLine

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace musicas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Deixa eu me apresentar");
+            Console.WriteLine("Que eu acabei de chegar");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Depois que me escutar");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Você vai lembrar meu nome");
+        }
+    }
+}
